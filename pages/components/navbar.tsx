1e399bc1aab4d7046f36ac3705a9439b3a38151a
hakuryo <hakuryo1001@protonmail.com>
2023-09-14 11:04:05 -0400

    navbar fix

--- pages/components/navbar.tsx
+++ pages/components/navbar.tsx
@@ -12,11 +12,11 @@ const Navbar: React.FC = () => {
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div className="flex h-16 justify-between">
+            <div className="flex h-16 md:h-20 justify-between">
               <div className="flex w-full">
                 <div className="-ml-2 mr-2 flex items-center md:hidden">
                   {/* Mobile menu button */}
-                  <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 bg-grey-800 hover:bg-grey-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  {/* <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 bg-grey-800 hover:bg-grey-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="absolute -inset-0.5" />
                     <span className="sr-only">Open main menu</span>
                     {open ? (
@@ -24,26 +24,14 @@ const Navbar: React.FC = () => {
                     ) : (
                       <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                     )}
-                  </Disclosure.Button>
+                  </Disclosure.Button> */}
                 </div>
                 <div className="flex flex-shrink-0 items-center">
                   <img
-                    className="h-8 w-auto"
+                    className="h-12 w-auto"
                     src="/img/logo.png"
                     alt="Your Company"
                   />
-                </div>
-                <div className="hidden w-full md:ml-6 md:flex md:items-center md:justify-center md:space-x-4">
-                  <a
-                    href="#"
-                    className={classNames(
-                      'bg-gray-900 text-white',
-                      'rounded-md px-3 py-2 text-sm font-medium'
-                    )}
-                    aria-current="page"
-                  >
-                    Purchase
-                  </a>
                 </div>
               </div>
               <div className="flex items-center">
@@ -53,64 +41,6 @@ const Navbar: React.FC = () => {
               </div>
             </div>
           </div>
-
-          <Disclosure.Panel className="md:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-              <a
-                href="#"
-                className={classNames(
-                  'bg-gray-900 text-white',
-                  'block rounded-md px-3 py-2 text-base font-medium'
-                )}
-                aria-current="page"
-              >
-                Purchase
-              </a>
-            </div>
-            <div className="border-t border-gray-700 pb-3 pt-4">
-              <div className="flex items-center px-5 sm:px-6">
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </div>
-                <div className="ml-3">
-                  <div className="text-base font-medium text-white">Tom Cook</div>
-                  <div className="text-sm font-medium text-gray-400">tom@example.com</div>
-                </div>
-                <button
-                  type="button"
-                  className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-              </div>
-              <div className="mt-3 space-y-1 px-2 sm:px-3">
-                <a
-                  href="#"
-                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                >
-                  Your Profile
-                </a>
-                <a
-                  href="#"
-                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                >
-                  Settings
-                </a>
-                <a
-                  href="#"
-                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                >
-                  Sign out
-                </a>
-              </div>
-            </div>
-          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
