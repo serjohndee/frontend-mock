3df80f787d41681b0657656d510f216dfa070c1e
hakuryo <hakuryo1001@protonmail.com>
2023-09-20 12:17:47 -0400

    wallet pop up fix attempt 2

--- components/connectWallet.tsx
+++ components/connectWallet.tsx
@@ -1,68 +1,78 @@
-import { useEffect, useState } from 'react'
+// https://onboard.blocknative.com/examples/connect-wallet
+import { useContext, useEffect, useState } from 'react'
 import { useConnectWallet } from '@web3-onboard/react'
 import { ethers } from 'ethers'
 import type {
-    TokenSymbol
-  } from '@web3-onboard/common'
+  TokenSymbol
+} from '@web3-onboard/common'
+import { ContractContext } from '../contexts'
 
-  
+
+
 interface Account {
-    address: string,
-    balance: Record<TokenSymbol, string> | null,
-    ens: {name: string|undefined, avatar: string|undefined}
+  address: string
+  balance: Record<TokenSymbol, string> | null,
+  // ens: {name: string|undefined, avatar: string|undefined}
+
 }
 
 export default function ConnectWallet() {
-  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
-  const [ethersProvider, setProvider] = useState<ethers.providers.Web3Provider | null>()
-  const [account, setAccount] = useState<Account | null>(null)
-  const [networkName, setNetworkName] = useState<string | null>(null)
+  const { wallet, account, connecting, connect, disconnect } = useContext(ContractContext);
+  // const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
+  // const [ethersProvider, setProvider] = useState<ethers.providers.Web3Provider | null>()
+  // const [account, setAccount] = useState<Account | null>(null)
+  // const [networkName, setNetworkName] = useState<string | null>(null)
 
-  
 
-  useEffect(() => {
-    if (!wallet) return;
-    if (wallet?.provider) {
-      const { name, avatar } = wallet?.accounts[0].ens ?? {}
-      setAccount({
-        address: wallet.accounts[0].address,
-        balance: wallet.accounts[0].balance,
-        ens: { name, avatar: avatar?.url }
-      })
-      
-    }
-  }, [wallet])
 
-  useEffect(() => {
-    if (!wallet) return;
-    // If the wallet has a provider than the wallet is connected
-    // if (connecting) {
-    if (wallet?.provider) {
-      const provider = new ethers.providers.Web3Provider(wallet.provider, 'any')
-      setProvider(provider)
-      // if using ethers v6 this is:
-      // ethersProvider = new ethers.BrowserProvider(wallet.provider, 'any')
 
-      // Get the network name
-      provider.getNetwork().then(network => setNetworkName(network.name))
-    }
-  }, [wallet])
-  
-  if(wallet?.provider && account) {
+  // useEffect(() => {
+  //   if (!wallet) return;
+  //   if (wallet?.provider) {
+  //     const { name, avatar } = wallet?.accounts[0].ens ?? {}
+  //     const address = wallet.accounts[0]?.address;
+
+  //     setAccount({
+  //       address: wallet.accounts[0].address
+  //       ,balance: wallet.accounts[0].balance
+  //       // , ens: { name, avatar: avatar?.url }
+  //     })
+  //    console.log("conenctWallet:", wallet.accounts[0].balance);
+  //   }
+  // }, [wallet])
+
+  // useEffect(() => {
+  //   if (!wallet) return;
+  //   // If the wallet has a provider than the wallet is connected
+  //   if (wallet?.provider) {
+  //     const provider = new ethers.providers.Web3Provider(wallet.provider, 'any')
+  //     setProvider(provider)
+  //     // if using ethers v6 this is:
+  //     // ethersProvider = new ethers.BrowserProvider(wallet.provider, 'any')
+
+  //     // Get the network name
+  //     provider.getNetwork().then(network => setNetworkName(network.name))
+  //   }
+  // }, [wallet])
+
+  if (wallet?.provider && account) {
     return (
       <div className="flex items-center">
-      <div className="flex-shrink-0 flex items-center gap-3">
-      {/* <div className="text-gray-50">Connected to {wallet.label} on {networkName} network</div> */}
-            {/* {account.ens?.avatar ? (<img src={account.ens?.avatar} alt="ENS Avatar" />) : null} */}
+        <div className="flex-shrink-0 flex items-center gap-3">
+          {/* <div className="text-gray-50">Connected to {wallet.label} on {networkName} network</div> */}
+          {/* {account.ens?.avatar ? (<img src={account.ens?.avatar} alt="ENS Avatar" />) : null} */}
           {/* <div>{ account.ens?.name ? account.ens.name : String(account.address).slice(0,10).concat("...") }</div> */}
           {/* <div>Connected to {wallet.label}</div> */}
           <button type="button"
-          className="relative inline-flex items-center gap-x-1.5 rounded-sm bg-gradient-to-r from-turquoise-500 to-lime-500 px-4 py-2 text-sm font-semibold text-grey-950 shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-          onClick={() => { disconnect({ label: wallet.label })}}>
-          { account.ens?.name ? account.ens.name : String(account.address).slice(0,10).concat("...") } 
-            </button>
+            className="relative inline-flex items-center gap-x-1.5 rounded-sm bg-gradient-to-r from-turquoise-500 to-lime-500 px-4 py-2 text-sm font-semibold text-grey-950 shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+            onClick={() => { disconnect({ label: wallet.label }) }}>
+            {String(account.address).slice(0, 10).concat("...")}
+            {/* {String(balanceNumber).slice(0, 7).concat(" Ξ")} */}
+
+            {/* { account.ens?.name ? account.ens.name : String(account.address).slice(0,10).concat("...") }  */}
+          </button>
         </div>
-        </div>
+      </div>
     )
   }
 
@@ -72,8 +82,8 @@ export default function ConnectWallet() {
         <button
           type="button"
           className="relative inline-flex items-center gap-x-1.5 rounded-sm bg-gradient-to-r from-turquoise-500 to-lime-500 px-4 py-2 text-sm font-semibold text-grey-950 shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-              disabled={connecting}
-              onClick={() => connect()}
+          disabled={connecting}
+          onClick={() => connect()}
         >
           Connect wallet
         </button>
@@ -81,3 +91,86 @@ export default function ConnectWallet() {
     </div>
   )
 }
+// import { useEffect, useState } from 'react'
+// import { useConnectWallet } from '@web3-onboard/react'
+// import { ethers } from 'ethers'
+// import type {
+//     TokenSymbol
+//   } from '@web3-onboard/common'
+
+  
+// interface Account {
+//     address: string,
+//     balance: Record<TokenSymbol, string> | null,
+//     ens: {name: string|undefined, avatar: string|undefined}
+// }
+
+// export default function ConnectWallet() {
+//   const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
+//   const [ethersProvider, setProvider] = useState<ethers.providers.Web3Provider | null>()
+//   const [account, setAccount] = useState<Account | null>(null)
+//   const [networkName, setNetworkName] = useState<string | null>(null)
+
+  
+
+//   useEffect(() => {
+//     if (!wallet) return;
+//     if (wallet?.provider) {
+//       const { name, avatar } = wallet?.accounts[0].ens ?? {}
+//       setAccount({
+//         address: wallet.accounts[0].address,
+//         balance: wallet.accounts[0].balance,
+//         ens: { name, avatar: avatar?.url }
+//       })
+      
+//     }
+//   }, [wallet])
+
+//   useEffect(() => {
+//     if (!wallet) return;
+//     // If the wallet has a provider than the wallet is connected
+//     // if (connecting) {
+//     if (wallet?.provider) {
+//       const provider = new ethers.providers.Web3Provider(wallet.provider, 'any')
+//       setProvider(provider)
+//       // if using ethers v6 this is:
+//       // ethersProvider = new ethers.BrowserProvider(wallet.provider, 'any')
+
+//       // Get the network name
+//       provider.getNetwork().then(network => setNetworkName(network.name))
+//     }
+//   }, [wallet])
+  
+//   if(wallet?.provider && account) {
+//     return (
+//       <div className="flex items-center">
+//       <div className="flex-shrink-0 flex items-center gap-3">
+//       {/* <div className="text-gray-50">Connected to {wallet.label} on {networkName} network</div> */}
+//             {/* {account.ens?.avatar ? (<img src={account.ens?.avatar} alt="ENS Avatar" />) : null} */}
+//           {/* <div>{ account.ens?.name ? account.ens.name : String(account.address).slice(0,10).concat("...") }</div> */}
+//           {/* <div>Connected to {wallet.label}</div> */}
+//           <button type="button"
+//           className="relative inline-flex items-center gap-x-1.5 rounded-sm bg-gradient-to-r from-turquoise-500 to-lime-500 px-4 py-2 text-sm font-semibold text-grey-950 shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+//           onClick={() => { disconnect({ label: wallet.label })}}>
+//           { account.ens?.name ? account.ens.name : String(account.address).slice(0,10).concat("...") } 
+//             </button>
+//         </div>
+//         </div>
+//     )
+//   }
+
+//   return (
+//     <div className="flex items-center">
+//       <div className="flex-shrink-0">
+//         <button
+//           type="button"
+//           className="relative inline-flex items-center gap-x-1.5 rounded-sm bg-gradient-to-r from-turquoise-500 to-lime-500 px-4 py-2 text-sm font-semibold text-grey-950 shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+//               disabled={connecting}
+//               onClick={() => connect()}
+//         >
+//           Connect wallet
+//         </button>
+//       </div>
+//     </div>
+//   )
+// }
--- contexts/index.tsx
+++ contexts/index.tsx
@@ -5,50 +5,174 @@ import { createContext, useContext, useEffect, useState } from 'react'
 
 import { ethers } from 'ethers'
 
-
-
-
-
+import { useConnectWallet } from '@web3-onboard/react'
 
 const ContractContext = createContext<any>({})
 
 
 const ContractProvider = ({ children }: any) => {
-const [provider, setProvider] = useState(null);
-const [signer, setSigner] = useState(null);
-const [accountAddress, setAccountAddress] = useState(null)
+    const [provider, setProvider] = useState(null); // This is for reading information unrelated to the wallet. 
+    const [walletProvider, setWalletProvider] = useState(null); // This is for ready specific user information.
+
+    const [signer, setSigner] = useState(null);
+    const [accountAddress, setAccountAddress] = useState(null);
+
+
+    /**
+     * Provider is fetched via ethers immeidately when page is loaded. 
+     * Wallet is fetched when connected.
+     * wallet.provider is not used.
+     * Signer is fetched when wallet is connected, killed when disconnected. 
+     */
+    // This could be used for non-wallet related reads. Differentiation will be simple.
     useEffect(() => {
-    if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
-    // We are in the browser and metamask is running
-    setProvider(new ethers.providers.Web3Provider(window.ethereum));
-    }
+        if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+            // We are in the browser and metamask is running
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            setProvider(provider);
+
+        }
     }, []);
 
 
-    useEffect(() => {
-        if (provider) {
-        setSigner(provider.getSigner());
-        }
-    }, [provider]);
+
+    // useEffect(() => {
+    //     if (provider) {
+    //         setSigner(provider.getSigner());
+    //     }
+    // }, [provider]);    
+
 
 
     useEffect(() => {
         if (!signer) return;
         const fetchAccountData = async () => {
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        setAccountAddress(accounts[0]);
+            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+            setAccountAddress(accounts[0]);
         }
         fetchAccountData();
     }, [signer])
 
 
+
+    // might conflict with the ones living in connectWallet
+    const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
+    // const [ethersProvider, setProvider] = useState<ethers.providers.Web3Provider | null>()
+    const [account, setAccount] = useState<Account | null>(null)
+
+    const [networkName, setNetworkName] = useState<string | null>(null)
+    /** balanceObject - the balance of accountAddress in the form of an object
+     * May be used to feed into contract calls.
+    */
+    const [balanceObject, setBalanceObject] = useState(0)
+    /** balanceNumber balanceObject converted into a number, divided by 10**18
+     * This should under NO circumstances be passed into any contracts. 
+     * This is only used for displays.
+     */
+    const [balanceNumber, setBalanceNumber] = useState(0)
+
+    useEffect(() => {
+        if (!provider) return;
+        const getWallet = () => {
+            if (wallet) {
+                console.log('set signer');
+                setSigner(provider.getSigner());
+            } else if (!wallet) {
+                console.log("remove signer!")
+                setSigner(null);
+            }
+        }
+        getWallet()
+    }, [provider, wallet])
+
+    //   useEffect(() => {
+    //     if (!provider || ! wallet) return;
+    //     if (wallet) {
+    //         setSigner(provider.getSigner());
+    //         console.log("signer set")
+    //     }
+    // }, [wallet, provider]);    
+
+    useEffect(() => {
+        if (!wallet) return;
+        if (wallet?.provider) {
+            // const { name, avatar } = wallet?.accounts[0].ens ?? {}
+            const address = wallet.accounts[0]?.address;
+            // const _balance = wallet.accounts[0]?.balance;
+            setAccount({
+                address: wallet.accounts[0].address
+                //   ,balance: wallet.accounts[0].balance
+                // , ens: { name, avatar: avatar?.url }
+            })
+        }
+    }, [wallet])
+
+    useEffect(() => {
+        if (!provider || !accountAddress) return;
+        const getBalance = async () => {
+            const _balanceObject = await provider.getBalance(accountAddress);
+            setBalanceObject(_balanceObject);
+
+            setBalanceNumber(Number(_balanceObject.toString()) / 10 ** 18)
+        }
+        getBalance();
+    }, [provider, accountAddress])
+
+    // Don't touch - raw wallet 
+    useEffect(() => {
+        if (!wallet) return;
+        // If the wallet has a provider than the wallet is connected
+        if (wallet?.provider) {
+            const provider = new ethers.providers.Web3Provider(wallet.provider, 'any')
+            // setProvider(provider)
+            // if using ethers v6 this is:
+            // ethersProvider = new ethers.BrowserProvider(wallet.provider, 'any')
+
+            // Get the network name
+            provider.getNetwork().then(network => setNetworkName(network.name))
+        }
+    }, [wallet])
+    // const [provider, setProvider] = useState(null);
+    // const [signer, setSigner] = useState(null);
+    // const [accountAddress, setAccountAddress] = useState(null)
+    // useEffect(() => {
+    //     if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+    //         // We are in the browser and metamask is running
+    //         setProvider(new ethers.providers.Web3Provider(window.ethereum));
+    //     }
+    // }, []);
+
+
+    // useEffect(() => {
+    //     if (provider) {
+    //         setSigner(provider.getSigner());
+    //     }
+    // }, [provider]);
+
+
+    // useEffect(() => {
+    //     if (!signer) return;
+    //     const fetchAccountData = async () => {
+    //         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    //         setAccountAddress(accounts[0]);
+    //     }
+    //     fetchAccountData();
+    // }, [signer])
+
+
     return (
         <ContractContext.Provider value={{
-        accountAddress
-        ,provider
+            accountAddress
+            , provider
+            , wallet
+            , account
+            , connecting
+            , connect
+            , disconnect
+            , balanceNumber
         }}>
             {children}
         </ContractContext.Provider>
-        );
+    );
 }
 export { ContractContext, ContractProvider }
